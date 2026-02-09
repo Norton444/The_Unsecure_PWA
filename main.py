4372c70340d2f7f22a06e5b3b7abcd72d5dcef5a
Norton444 <norton.lu1@education.nsw.gov.au>
2026-02-09 13:09:25 +1100

    deleted test

--- main.py
+++ main.py
@@ -12,8 +12,6 @@ app = Flask(__name__)
 # Enable CORS to allow cross-origin requests (needed for CSRF demo in Codespaces)
 CORS(app)
 
-# HI HI HI HI
-
 
 @app.route("/success.html", methods=["POST", "GET", "PUT", "PATCH", "DELETE"])
 def addFeedback():
